cc92a4096288bbe00b05a18a81fd2d9e5e9b753f
herbwood <herbwood27@gmail.com>
2020-07-30 15:35:23 +0900

    android development settings done

--- app/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
@@ -72,7 +72,7 @@ public abstract class Classifier {
 
   /** An instance of the driver class to run model inference with Tensorflow Lite. */
   // TODO: Declare a TFLite interpreter
-
+  protected Interpreter tflite;
 
   /** Options for configuring the Interpreter. */
   private final Interpreter.Options tfliteOptions = new Interpreter.Options();
@@ -187,7 +187,7 @@ public abstract class Classifier {
     tfliteOptions.setNumThreads(numThreads);
 
     // TODO: Create a TFLite interpreter instance
-
+    tflite = new Interpreter(tfliteModel, tfliteOptions);
 
     // Loads labels out from the label file.
     labels = FileUtil.loadLabels(activity, getLabelPath());
@@ -231,6 +231,7 @@ public abstract class Classifier {
     Trace.beginSection("runInference");
     long startTimeForReference = SystemClock.uptimeMillis();
     // TODO: Run TFLite inference
+    tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
 
     long endTimeForReference = SystemClock.uptimeMillis();
     Trace.endSection();
@@ -239,7 +240,9 @@ public abstract class Classifier {
     // Gets the map of label and probability.
     // TODO: Use TensorLabel from TFLite Support Library to associate the probabilities
     //       with category labels
-
+    Map<String, Float> labeledProbability =
+            new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
+                    .getMapWithFloatValue();
     Trace.endSection();
 
     // Gets top-k results.
@@ -253,7 +256,8 @@ public abstract class Classifier {
 
     }
     // TODO: Close the GPU delegate
-
+    tflite.close();
+    tflite = null;
 
     tfliteModel = null;
   }
@@ -279,11 +283,11 @@ public abstract class Classifier {
     // TODO: Define an ImageProcessor from TFLite Support Library to do preprocessing
     ImageProcessor imageProcessor =
             new ImageProcessor.Builder()
-
-
-
-
-                .build();
+                    .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                    .add(new ResizeOp(imageSizeX, imageSizeY, ResizeMethod.NEAREST_NEIGHBOR))
+                    .add(new Rot90Op(numRoration))
+                    .add(getPreprocessNormalizeOp())
+                    .build();
     return imageProcessor.process(inputImageBuffer);
   }
 
